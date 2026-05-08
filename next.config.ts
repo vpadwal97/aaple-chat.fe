ad3523d34b4b9fd4dcf4b5a2774c4ea19867a8ba
phoenixvp <94145215+PHOENIXVP@users.noreply.github.com>
2026-05-08 10:00:10 +0000

    running site

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["*.csb.app", "*.vercel.app"],
 };
 
 export default nextConfig;
